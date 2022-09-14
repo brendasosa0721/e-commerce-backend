741003b008709078949f5cc44691e7e606de3a5e
Brenda  Sosa <brenda.sosa@gmail.com>
2022-09-14 11:36:05 -0400

    added columns into the models/category

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
