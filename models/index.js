9f35c8b562a5e0cefd311cc75a4556b87faee708
Brenda  Sosa <brenda.sosa@gmail.com>
2022-09-14 11:59:02 -0400

    define columns in models/product,tag,producttag

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "Category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "Category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Category.belongsToMany(Tag, {
+  through: "ProductTag"
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: "ProductTag"
+})
 module.exports = {
   Product,
   Category,
